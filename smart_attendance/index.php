dae0bb29c7640f37cf7ed921e85fa87b8cbac6e3
Akash Jaiswal <akashjas1996@gmail.com>
2019-12-05 18:42:18 +0530

    need

--- smart_attendance/index.php
+++ smart_attendance/index.php
@@ -61,9 +61,9 @@ mkdir("cache");
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>MODERATOR | HIGHRADIUS</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/mdb.min.css" rel="stylesheet">
-  <link href="css/style.min.css" rel="stylesheet">
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/mdb.min.css" rel="stylesheet">
+  <link href="../css/style.min.css" rel="stylesheet">
   <link rel="icon" href="img/favicon.png">
   <style>
 
@@ -211,13 +211,13 @@ position:absolute;
     </div>
   </main>
   <?php include '../inc/footer.php' ?>
-  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
   <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="../js/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="../js/mdb.min.js"></script>
   <!-- Initializations -->
   <script type="text/javascript">
     // Animations initialization
